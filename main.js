afec44e408801af28b19c1ce9281af1f9524c62e
cawauchi6204 <cawauchi6204@gmail.com>
2020-05-14 12:41:58 +0900

    chapter12まで

--- main.js
+++ main.js
@@ -1,25 +1,93 @@
-// spinボタンをおすとパネルが動き始める
-const spinBtn = document.getElementById('spin');
-const panel = document.getElementsByClassName('panel');
+'use strict';
 
-spinBtn.addEventListener('click' , () => {
-  setInterval(() => {
-    let panelImg = panel.getAttribute('src');
-    console.log(panel);
-  },1000)
-});
+{
+  class Panel {
+    constructor() {
+      const section = document.createElement('section');
+      section.classList.add('panel');
 
-// valueを取得したい
+      this.img = document.createElement('img');
+      this.img.src = this.getRandomImage();
 
+      this.timeoutId = undefined;
 
-// stopボタンを押すとパネルがストップする
-const stopBtn = document.getElementsByClassName('stop');
-stopBtn.addEventListener('click' , () => {
-  const stopN = panel.value;
-  let panelNumber = this.stopN;
+      this.stop = document.createElement('div');
+      this.stop.textContent = 'STOP';
+      this.stop.classList.add('stop');
+      this.stop.addEventListener('click', () => {
+        if(this.stop.classList.contains('inactive')) {
+          return;
+        }
+        this.stop.classList.add('inactive');
+        clearTimeout(this.timeoutId);
 
+        panelsLeft--;
 
-});
-// getElementsByClassName()メソッドが返す値は配列だと思っていたら、実は違った！
-// getElementsByClassName()は、配列に似ていますが実際には配列ではないHTMLcollectionオブジェクトを返します。
-// 戻り値	elements	存在する場合は1個以上のHTMLCollectionを返し、存在しない場合は0個のHTMLCollectionを返す
+        if (panelsLeft === 0) {
+          checkResult();
+        }
+      });
+
+      section.appendChild(this.img);
+      section.appendChild(this.stop);
+
+      const main = document.querySelector('main');
+      main.appendChild(section);
+    }
+
+    getRandomImage() {
+      const images = [
+        'img/seven.png',
+        'img/bell.png',
+        'img/cherry.png',
+      ];
+      return images[Math.floor(Math.random() * images.length)];
+    }
+
+    spin() {
+      this.img.src = this.getRandomImage();
+      this.timeoutId = setTimeout(() => {
+        this.spin();
+      }, 50);
+    }
+
+    isUnmatched(p1,p2) {
+      return this.img.src !== p1.img.src && this.img.src !== p2.img.src;
+    }
+
+    unmatch() {
+      this.img.classList.add('unmatched');
+    }
+}
+
+  function checkResult() {
+    if (panels[0].isUnmatched(panels[1], panels[2])) {
+      panels[0].unmatch();
+    }
+    if (panels[1].isUnmatched(panels[0], panels[2])) {
+      panels[1].unmatch();
+    }
+    if (panels[2].isUnmatched(panels[0], panels[1])) {
+      panels[2].unmatch();
+    }
+  }
+
+  const panels = [
+    new Panel(),
+    new Panel(),
+    new Panel(),
+  ];
+
+  let panelsLeft = 3;
+
+  const spin = document.getElementById('spin');
+  spin.addEventListener('click', () => {
+    if(spin.classList.contains('inactive')) {
+      return;
+    }
+    spin.classList.add('inactive');
+    panels.forEach(panel => {
+      panel.spin();
+    });
+  });
+}
